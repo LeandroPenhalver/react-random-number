2950fd12a3e32850c8e7dbe2b9ecdcc79574714f
LeandroPenhalver <38468347+LeandroPenhalver@users.noreply.github.com>
2020-08-24 22:04:46 -0300

    Update index.js

--- src/componentes/PaginaInicial/index.js
+++ src/componentes/PaginaInicial/index.js
@@ -4,12 +4,16 @@ import './estilo.css';
 export default function PaginaInicial() {
     const[randomNumber, setRandomNumber] = useState(0);
 
+    function randomNumber(){
+        setRanromNumber(Math.floor(Math.random() * (100-1) + 1));
+    }
+
     return (
         <main>
             <h1>Número Aleatório</h1>
             <h2>{randomNumber}</h2>
             <p>Clique no botão abaixo para gerar um número aleatório</p>
-            <button>
+            <button onClick={ randomNumber }>
                 Gerar Número
             </button>
         </main>
